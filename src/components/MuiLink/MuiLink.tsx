60b9ad5c762000f78954c88ddcb369b33c7aaf64
maciekbe1 <madecki1991@gmail.com>
2023-03-14 11:17:35 +0100

    fix: link forward ref error

--- src/components/MuiLink/MuiLink.tsx
+++ src/components/MuiLink/MuiLink.tsx
@@ -1,8 +1,16 @@
 import { Link as MuiLink, LinkProps } from "@mui/material";
+import { forwardRef } from "react";
 import { Link as ReactRouterLink } from "react-router-dom";
 
-export const Link: React.FC<LinkProps> = (props) => {
-  return (
-    <MuiLink {...props} component={ReactRouterLink} to={props.href ?? "#"} />
-  );
-};
+export const Link = forwardRef<HTMLAnchorElement, LinkProps>(
+  function LinkComposed(props, ref) {
+    return (
+      <MuiLink
+        {...props}
+        component={ReactRouterLink}
+        to={props.href ?? "#"}
+        ref={ref}
+      />
+    );
+  }
+);
