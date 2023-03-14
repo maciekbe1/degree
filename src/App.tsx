a8c577ae5c820c6815972208a109d717aaa2452e
maciekbe1 <madecki1991@gmail.com>
2023-03-14 12:05:20 +0100

    fix: handle routes in app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { ThemeProvider } from "@mui/material/styles";
-import { Suspense } from "react";
+import { lazy, Suspense } from "react";
 import { Provider } from "react-redux";
-import { RouterProvider } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 
+import { Layout } from "@/components";
 import { persistor, store } from "@/store";
 
-import router from "./router";
+const Home = lazy(() => import("./pages/Home"));
+const Product = lazy(() => import("./pages/Product"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+
 import theme from "./theme";
 
 function App() {
@@ -15,7 +19,15 @@ function App() {
       <PersistGate loading={null} persistor={persistor}>
         <ThemeProvider theme={theme}>
           <Suspense fallback={null}>
-            <RouterProvider router={router} />
+            <BrowserRouter>
+              <Layout>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/product/:id" element={<Product />} />
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+              </Layout>
+            </BrowserRouter>
           </Suspense>
         </ThemeProvider>
       </PersistGate>
